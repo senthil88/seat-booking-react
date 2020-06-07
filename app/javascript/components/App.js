41e2b2fd346dc22534db3d401b76ba99ee793091
skumar <rask.kumar@gmail.com>
2020-06-07 21:29:23 +0530

    Load bootstrap

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from './layout/Header';
 import Movie from 'components/movie/Movie';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap'
 
 class App extends React.Component {
